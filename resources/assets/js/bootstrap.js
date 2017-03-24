639630280b8bd931062b9870d72c3d82badecc47
an.smirnov <sandark7@gmail.com>
2017-03-24 20:49:47 +0300

    Sass to stylus

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -3,11 +3,11 @@ window._ = require('lodash');
 
 window.$ = window.jQuery = require('jquery');
 
-require('bootstrap-sass');
+require('bootstrap-styl');
 
 window.axios = require('axios');
 
 window.axios.defaults.headers.common = {
-    'X-CSRF-TOKEN': window.Laravel.csrfToken,
+    // 'X-CSRF-TOKEN': window.Laravel.csrfToken,
     'X-Requested-With': 'XMLHttpRequest'
 };
