771ad00cd4ef3d04518364af667efc55a1964553
Shchegolskov Petr <p.shchegolskov@rambler-co.ru>
2017-03-25 09:37:07 +0300

    admin

--- app/Http/Admin/Offer.php
+++ app/Http/Admin/Offer.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Admin;
 
-use SleepingOwl\Admin\Contracts\Display\DisplayInterface;
-use SleepingOwl\Admin\Contracts\Form\FormInterface;
+use AdminColumn;
+use AdminDisplay;
+use AdminForm;
+use AdminFormElement;
+use SleepingOwl\Admin\Contracts\DisplayInterface;
+use SleepingOwl\Admin\Contracts\FormInterface;
+use SleepingOwl\Admin\Contracts\Initializable;
 use SleepingOwl\Admin\Section;
 
 /**
@@ -13,8 +18,14 @@ use SleepingOwl\Admin\Section;
  *
  * @see http://sleepingowladmin.ru/docs/model_configuration_section
  */
-class Offer extends Section
+class Offer extends Section implements Initializable
 {
+
+    /**
+     * @var \App\Offer
+     */
+    protected $model;
+
     /**
      * @see http://sleepingowladmin.ru/docs/model_configuration#ограничение-прав-доступа
      *
@@ -33,11 +44,49 @@ class Offer extends Section
     protected $alias;
 
     /**
+     * Initialize class.
+     */
+    public function initialize()
+    {
+        // Добавление пункта меню и счетчика кол-ва записей в разделе
+        $this->addToNavigation($priority = 500, function() {
+            return \App\Offer::count();
+        });
+
+        $this->creating(function($config, \Illuminate\Database\Eloquent\Model $model) {
+        });
+    }
+
+    /**
+     * @return string|\Symfony\Component\Translation\TranslatorInterface
+     */
+    public function getTitle()
+    {
+        return 'Предложения';
+    }
+
+    /**
      * @return DisplayInterface
      */
     public function onDisplay()
     {
-        // todo: remove if unused
+        $display = AdminDisplay::table()->setColumns([
+            AdminColumn::link('name')->setLabel('Название')->setWidth('400px'),
+            //AdminColumn::text('description')->setLabel('Описание')->setAttribute('class', 'text-muted')
+        ]);
+        $display->paginate(15);
+        return $display;
+    }
+
+    // Create And Edit
+    public function onCreateAndEdit()
+    {
+        $form = AdminForm::panel()->addBody(
+            AdminFormElement::text('name', 'Название')->required()->unique()
+            //AdminFormElement::textarea('description', 'Описание')->setRows(2)
+            //AdminFormElement::text('phone', 'Phone')
+        );
+        return $form;
     }
 
     /**
@@ -45,10 +94,12 @@ class Offer extends Section
      *
      * @return FormInterface
      */
+    /*
     public function onEdit($id)
     {
         // todo: remove if unused
     }
+    */
 
     /**
      * @return FormInterface
--- app/Http/Sections/Users.php
+++ app/Http/Sections/Users.php
@@ -2,42 +2,45 @@
 
 namespace App\Http\Sections;
 
-use SleepingOwl\Admin\Contracts\Display\DisplayInterface;
-use SleepingOwl\Admin\Contracts\Form\FormInterface;
+use AdminColumn;
+use AdminDisplay;
+use AdminForm;
+use AdminFormElement;
+use App\Role;
+use SleepingOwl\Admin\Contracts\DisplayInterface;
+use SleepingOwl\Admin\Contracts\FormInterface;
 use SleepingOwl\Admin\Section;
 
-/**
- * Class Users
- *
- * @property \App\User $model
- *
- * @see http://sleepingowladmin.ru/docs/model_configuration_section
- */
 class Users extends Section
 {
+
     /**
-     * @see http://sleepingowladmin.ru/docs/model_configuration#ограничение-прав-доступа
-     *
      * @var bool
      */
-    protected $checkAccess = false;
+    protected $checkAccess = true;
 
     /**
      * @var string
      */
-    protected $title;
+    protected $title = 'Users';
 
     /**
      * @var string
      */
-    protected $alias;
+    protected $alias = 'users';
 
     /**
      * @return DisplayInterface
      */
     public function onDisplay()
     {
-        // todo: remove if unused
+        return AdminDisplay::table()
+            ->with('roles')
+            ->setHtmlAttribute('class', 'table-primary')
+            ->setColumns(
+                AdminColumn::link('name')->setLabel('Username'),
+                AdminColumn::email('email')->setLabel('Email')->setWidth('150px')
+            )->paginate(20);
     }
 
     /**
@@ -47,7 +50,11 @@ class Users extends Section
      */
     public function onEdit($id)
     {
-        // todo: remove if unused
+        return AdminForm::panel()->addBody([
+            AdminFormElement::text('name', 'Username')->required(),
+            AdminFormElement::password('password', 'Password')->required()->addValidationRule('min:6'),
+            AdminFormElement::text('email', 'E-mail')->required()->addValidationRule('email'),
+        ]);
     }
 
     /**
@@ -56,21 +63,5 @@ class Users extends Section
     public function onCreate()
     {
         return $this->onEdit(null);
-    }
-
-    /**
-     * @return void
-     */
-    public function onDelete($id)
-    {
-        // todo: remove if unused
-    }
-
-    /**
-     * @return void
-     */
-    public function onRestore($id)
-    {
-        // todo: remove if unused
     }
 }
