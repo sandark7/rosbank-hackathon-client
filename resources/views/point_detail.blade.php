ec7d97433dc2fa855336c02651102848ef5298d0
an.smirnov <sandark7@gmail.com>
2017-03-25 19:30:29 +0300

    Added popup

--- resources/views/point_detail.blade.php
+++ resources/views/point_detail.blade.php
@@ -1,83 +1,105 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Торговая точка</h4>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Торговая точка</h4>
+          </div>
+
+          <div class="panel-body">
+            <p>Адрес: {{ $point->address }}</p>
+            <p>Терминалы: {{ $point->getTerminalIds() }}</p>
+          </div>
+
+          <div class="panel-body">
+            <p>Анализ торговой точки не проводился</p>
+            <button id="popupOpen" class="btn btn-success pull-right" data-toggle="modal" data-target="#popup">Проанализировать</button>
+            <div id="popup" class="modal" tabindex="-1" role="dialog" aria-labelledby="popupLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h4 class="modal-title" id="popupLabel">Анализ покупок</h4>
+                  </div>
+                  <div class="modal-body">
+                    <div id="step_1" class="step">
+                      <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                      <svg class="spinner">
+                        <use xlink:href="#icon-spin"></use>
+                      </svg>
+                      <span class="step__text">Ищу покупки в этой точке</span>
+                      <span class="step__count">({{ $steps['step_1'] }})</span>
                     </div>
-
-                    <div class="panel-body">
-                        <p>Адрес: {{ $point->address }}</p>
-                        <p>Терминалы: {{ $point->getTerminalIds() }}</p>
+                    <div id="step_2" class="step is-hidden">
+                      <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                      <svg class="spinner">
+                        <use xlink:href="#icon-spin"></use>
+                      </svg>
+                      <span class="step__text">Ищу аккаунты клиентов</span>
+                      <span class="step__count">({{ $steps['step_2'] }})</span>
                     </div>
-
-                    <div class="panel-body">
-                        <p>Анализ торговой точки не проводился</p>
-                        <p><a href="#" onclick="javascript:document.getElementById('popup').style.display='block'"
-                              class="btn btn-success pull-right">Проанализировать</a></p>
-                        <div id="popup" style="display: none; border: 2px solid green;">
-                            Попап с процессом подсчета характеристик
-                            <br>
-                            <ul>
-                                <li>1</li>
-                                <li>2</li>
-                                <li>3</li>
-                                <li>4</li>
-                            </ul>
-                            <a href="#"
-                               onclick="javascript:document.getElementById('popup').style.display='none'">Закрыть</a>
-                        </div>
+                    <div id="step_3" class="step is-hidden">
+                      <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                      <svg class="spinner">
+                        <use xlink:href="#icon-spin"></use>
+                      </svg>
+                      <span class="step__text">Анализирую все их покупки</span>
                     </div>
-
-                    <div class="panel-body">
-                        <p>Аналитика торговой точки от {{ $point->analytic_date }}</p>
-                        <p><a href="#" onclick="javascript:document.getElementById('popup').style.display='block'"
-                              class="btn btn-success pull-right">Проанализировать еще раз</a></p>
-
-                        <table class="table">
-                            <thead><tr>
-                                <th>Параметр</th>
-                                <th>Значение</th>
-
-                            </tr> </thead>
-
-                            <tbody>
-                                <tr>
-                                    <td>Средний чек</td>
-                                    <td>10</td>
-                                </tr>
-                                <tr>
-                                    <td>Средний чек</td>
-                                    <td>10</td>
-                                </tr>
-                                <tr>
-                                    <td>Средний чек</td>
-                                    <td>10</td>
-                                </tr>
-                                <tr>
-                                    <td>Средний чек</td>
-                                    <td>10</td>
-                                </tr>
-
-                            </tbody>
-                        </table>
-
-                    </div>
-
-                    <div class="panel-body">
-                        <p>Сделать предложение</p>
-                        <p>
-                            <select name="offer">
-                                <option></option>
-                            </select>
-                        </p>
-                    </div>
-
+                  </div>
+                  <div class="modal-footer">
+                    <button id="cancelPopup" type="button" class="btn btn-default" data-dismiss="modal">Отменить</button>
+                    <button id="closePopup" type="button" class="btn btn-success is-hidden" data-dismiss="modal">Готово</button>
+                  </div>
                 </div>
+              </div>
             </div>
+
+          <div class="panel-body">
+            <p>Аналитика торговой точки от {{ $point->analytic_date }}</p>
+
+            <table class="table">
+              <thead><tr>
+                <th>Параметр</th>
+                <th>Значение</th>
+
+              </tr> </thead>
+
+              <tbody>
+                <tr>
+                  <td>Средний чек</td>
+                  <td>10</td>
+                </tr>
+                <tr>
+                  <td>Средний чек</td>
+                  <td>10</td>
+                </tr>
+                <tr>
+                  <td>Средний чек</td>
+                  <td>10</td>
+                </tr>
+                <tr>
+                  <td>Средний чек</td>
+                  <td>10</td>
+                </tr>
+
+              </tbody>
+            </table>
+
+          </div>
+
+          <div class="panel-body">
+            <p>Сделать предложение</p>
+            <p>
+              <select name="offer">
+                <option></option>
+              </select>
+            </p>
+          </div>
+
         </div>
+      </div>
     </div>
+  </div>
 @endsection
