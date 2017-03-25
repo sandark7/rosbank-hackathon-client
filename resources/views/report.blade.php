8fd29f0ee96695df92c40b1de851a686493f363d
Shchegolskov Petr <p.shchegolskov@rambler-co.ru>
2017-03-25 19:41:01 +0300

    reports

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h4>Отчеты точек</h4>
+                        <h4>Отчеты по торговым точкам</h4>
                     </div>
 
                     <div class="panel-body">
