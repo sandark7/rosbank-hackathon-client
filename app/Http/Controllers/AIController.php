22fffc020cddd9fcdf4aa5202f0ecfe34b07c1ec
Shchegolskov Petr <p.shchegolskov@rambler-co.ru>
2017-03-25 17:50:53 +0300

    api bigdata

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Offer;
+use App\Point;
 use Illuminate\Support\Facades\Validator;
 
 class AIController extends Controller
@@ -17,6 +17,9 @@ class AIController extends Controller
      */
     public function indexByPoint($point_id)
     {
+
+        Point::where('id', $point_id)
+            ->update(['analytic_date' => date('Y-m-d H:i:s')]);
 
         $bigdata = [
             'Параметр 1' => rand(10, 200),
