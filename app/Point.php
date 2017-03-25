791ebba71ac3caf414944db7b5b1c2c64f4aeacb
Shchegolskov Petr <p.shchegolskov@rambler-co.ru>
2017-03-25 21:38:33 +0300

    correct bigdata

--- app/Point.php
+++ app/Point.php
@@ -39,20 +39,20 @@ class Point extends Model
 
             'average_check' => [
                 'my' => rand(451, 501),
-                'others' => rand(652, 702)
+                'others' => rand(552, 802)
             ],
             'transactions' => [
                 'my' => rand(100, 149) / 100,
                 'others' => rand(151, 200) / 100
             ],
             'spending' => [
-                'my' => rand(351, 451) ,
-                'others' => rand(971, 1001)
+                'my' => rand(151, 451) ,
+                'others' => rand(771, 1001)
             ],
 
-            'potential' => rand(201, 230),
-            'sleep' => rand(142, 182),
-            'first' => rand(349, 451),
+            'potential' => rand(101, 230),
+            'sleep' => rand(122, 182),
+            'first' => rand(249, 451),
 
         ];
 
